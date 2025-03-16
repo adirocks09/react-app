a414dddaa77b0cd8984f4813682d02d45b8ae5ad
adirocks09 <adi9892@gmail.com>
2025-03-16 23:31:08 +0530

    ReduxJs Toolkit
    ReduxJs Toolkit

--- basic-react-app/src/store/counterSlice.js
+++ basic-react-app/src/store/counterSlice.js
@@ -11,17 +11,17 @@ const counterSlice = createSlice({
     decrement: (state, action) => {
       // action is optional here since there is no payload (undefined)
       console.log("decrement action : ");
-      console.log(action);
+      console.log(action); // {type: 'counterSlice/decrement', payload: undefined}
       state.value--;
     },
     add: (state, action) => {
       console.log("add action : ");
-      console.log(action);
+      console.log(action); // {type: 'counterSlice/add', payload: '99'}
       state.value += Number(action.payload);
     },
     subtract: (state, action) => {
       console.log("subtract action : ");
-      console.log(action);
+      console.log(action); // {type: 'counterSlice/subtract', payload: '66'}
       state.value -= Number(action.payload);
     },
   },
